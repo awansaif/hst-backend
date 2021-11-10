8e07472bbb5a002b86660b44a6353fbd70631e9d
Awan <m.awan.saifurrehman@gmail.com>
2021-11-10 16:58:42 +0500

    resolve error in cache clear trait

--- app/Traits/CacheClear.php
+++ app/Traits/CacheClear.php
@@ -1,18 +1,22 @@
+<?php
+
 namespace App\Traits;
+
+use Illuminate\Support\Facades\Artisan;
 
 trait CacheClear
 {
-protected static function boot()
-{
-parent::boot();
-static::updated(function () {
-Artisan::call('cache:clear');
-});
-static::created(function () {
-Artisan::call('cache:clear');
-});
-static::saved(function () {
-Artisan::call('cache:clear');
-});
-}
+    protected static function boot()
+    {
+        parent::boot();
+        static::updated(function () {
+            Artisan::call('cache:clear');
+        });
+        static::created(function () {
+            Artisan::call('cache:clear');
+        });
+        static::saved(function () {
+            Artisan::call('cache:clear');
+        });
+    }
 }
