2edb145d75bf207ef6218b3aadfb2a2a96dd4d85
Awan <m.awan.saifurrehman@gmail.com>
2021-11-14 14:55:52 +0500

    add cache on search api nd limit return data

--- app/Http/Controllers/Api/v2/SearchController.php
+++ app/Http/Controllers/Api/v2/SearchController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\v2;
 use App\Http\Controllers\Controller;
 use App\Models\Blog;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class SearchController extends Controller
 {
@@ -17,9 +18,16 @@ class SearchController extends Controller
     public function __invoke(Request $request)
     {
         // search blog
-        $blogs = Blog::where('title', 'like', '%' . $request->q . '%')
-            ->orWhere('body', 'like', '%' . $request->q . '%')
-            ->paginate(10);
+        if (Cache::get('search_blog_' . $request->q)) {
+            $blogs = Cache::get('search_blog_' . $request->q);
+        } else {
+            $blogs = Blog::where('title', 'like', '%' . $request->q . '%')
+                ->orWhere('body', 'like', '%' . $request->q . '%')
+                ->select('title', 'featured_image', 'slug', 'editor_id', 'created_at', 'id', 'editor:id,names')
+                ->orderBy('id', 'DESC')
+                ->paginate(10);
+            Cache::put('search_blog_' . $request->q, $blogs, 120);
+        }
         return response()->json($blogs);
     }
 }
